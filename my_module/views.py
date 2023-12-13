60697d1b9c2d48a44b7d46f522c1beb1d8546f0d
stoliarchuk1 <99402530+stoliarchuk1@users.noreply.github.com>
2023-12-13 16:54:49 +0200

    Update views.py

--- my_module/views.py
+++ my_module/views.py
@@ -1,5 +1,9 @@
 from my_module import app
 
+@app.route('/')
+def healthcheck():
+    return 'Server is healthy!'
+    
 @app.route('/healthcheck')
 def healthcheck():
     return 'Server is healthy!'
